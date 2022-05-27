170f90cf38b4493beaeec849e9c933cac7eac8cf
Denys-Roman <rudykdenys@gmail.com>
2022-05-27 11:47:42 +0300

    Hide unused button

--- ImageToText/Form1.Designer.cs
+++ ImageToText/Form1.Designer.cs
@@ -210,6 +210,7 @@
             this.button5.TabIndex = 14;
             this.button5.Text = "Open Image Folder";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // label6
